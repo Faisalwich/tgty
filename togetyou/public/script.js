3b2c90e7ca92a5cf6640b9b3197091baabca50a7
Faisal Wicahyono <110772823+Faisalwich@users.noreply.github.com>
2025-02-08 15:22:21 +0700

    Delete togetyou directory

--- togetyou/public/script.js
+++
@@ -1,7 +0,0 @@
-// Alert saat tombol diklik
-document.getElementById("rButton").addEventListener("click", function () {
-  alert("Aku benar-benar mengagumi kamu! 💖");
-});
-
-// Kirim request ke server saat website dibuka
-fetch("/track");
